283214d48479c474c13392278bc9c996c1ee80c3
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-02-01 12:26:09 -0800

    Menu Development
    Added a Menu scene that will eventually hold the main menu. The buttons don't really do anything, but the UI elements are all there.
    
    Also added MenuTesting.cs, which is a simple script that pings the console. I'm using it to make sure buttons actually do things when pressed.

---
+++ Unity Prototype/Assets/Scripts/MenuTesting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTesting : MonoBehaviour
+    
+{
+    //The only thing this script does is print 'Ping!' to the console when the public method is called.
+    public void PingMe()
+    {
+        Debug.Log("Ping!");
+    }
+
+    public void PingMeTwo()
+    {
+        Debug.Log("PING ME BOY");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("This would quite the game, but Application.Quit doesn't work in editor");
+        Application.Quit();
+    }
+}
