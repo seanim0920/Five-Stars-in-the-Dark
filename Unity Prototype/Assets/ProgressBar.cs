1d6ec5b018d0d1c08053a353f0edc59372b4ef9d
sean <seanim0920@gmail.com>
2020-02-03 11:59:03 -0800

    end screen scene with score display

---
+++ Unity Prototype/Assets/ProgressBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    Slider scoreBar;
+    // Start is called before the first frame update
+    float shakeAmount = 20;
+    float shakeOffset = 0;
+    float duration = 1f;
+    float score = 1f;
+    float lerpTime = 0;
+    RectTransform rect;
+    void Start()
+    {
+        scoreBar = GetComponentInChildren<Slider>();
+        rect = GetComponent<RectTransform>();
+        StartCoroutine(IncrementProgress());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(lerpTime);
+        scoreBar.value = lerpTime;
+
+        Vector2 displacement = Quaternion.Euler(0, 0, Random.Range(0, 360)) * Vector2.right * shakeOffset;
+        rect.anchoredPosition = Vector2.zero + displacement;
+    }
+
+    IEnumerator IncrementProgress()
+    {
+        while (lerpTime < 0.99f)
+        {
+            lerpTime = Mathf.Sin(Time.time);
+            //float lerpTime = Mathf.PingPong(Time.time, duration) / duration;
+            //Debug.Log(lerpTime);
+            //smooth interpolation dependso n smothness of time change
+            shakeOffset = Mathf.Lerp(shakeAmount, 0, scoreBar.value / score);
+            yield return new WaitForSeconds(0);
+        }
+    }
+}
--- Unity Prototype/Assets/Proximity.cs
+++ Unity Prototype/Assets/Proximity.cs
@@ -23,8 +23,30 @@ public class Proximity : MonoBehaviour
         {
             float distance = frontSight.collider.gameObject.transform.position.y - transform.position.y;
             beep.volume = 1;
-            beep.pitch = 0.333f*Mathf.Pow(distance-3, 2) + 1;
+            beep.pitch = 0.33f*Mathf.Pow(distance-3, 2) + 1f;
             //beep.pitch = -(3/eyesight)*distance + 3 + 1;
         }
     }
+
+    /*
+     * 
+        RaycastHit2D sightLeftSide = Physics2D.Raycast(transform.position - new Vector3(0.25f, 0, 0), transform.up, eyesight);
+        RaycastHit2D sightRightSide = Physics2D.Raycast(transform.position + new Vector3(0.25f, 0, 0), transform.up, eyesight);
+        //checks the front of the car on the left and right. If the beeping doesn't play, the car is free to move ahead.
+        //beep.volume = 0;
+        float leftDistance = 1;
+        float rightDistance = 1;
+        if (sightLeftSide.collider.gameObject.tag == "Car")
+        {
+            leftDistance = Vector3.Magnitude(sightLeftSide.collider.gameObject.transform.position - transform.position);
+        }
+        if (sightRightSide.collider.gameObject.tag == "Car")
+        {
+            rightDistance = Vector3.Magnitude(sightRightSide.collider.gameObject.transform.position - transform.position);
+        }
+        float distance = Mathf.Min(leftDistance, rightDistance);
+        //beep.volume = 1;
+        beep.pitch = 0.333f * Mathf.Pow(distance - 3, 2) + 1f;
+        //beep.pitch = -(3/eyesight)*distance + 3 + 1;
+        */
 }
