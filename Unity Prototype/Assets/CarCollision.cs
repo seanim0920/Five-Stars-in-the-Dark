428788a21777bea64ad0056f0e4ab7ec07bcced0
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-02-19 18:10:36 -0800

    Merge Revert
    A lot of stuff got messed up when I tried to merge and honestly I don't want to deal with it.

---
+++ Unity Prototype/Assets/CarCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollision : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private AudioClip bump;
+    public GameObject hitsound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bump = Resources.Load<AudioClip>("Audio/hit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Car")
+        {
+            hitsound.transform.position = transform.position;
+            hitsound.GetComponent<AudioSource>().Play();
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Car")
+        {
+            hitsound.transform.position = transform.position;
+            hitsound.GetComponent<AudioSource>().Play();
+        }
+    }
+}
