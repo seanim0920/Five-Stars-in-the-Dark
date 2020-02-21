03fb8d0f0e9283389844d7712b477f9c101c72e8
sean <seanim0920@gmail.com>
2020-02-20 16:34:29 -0800

    Removed old scripts: Movement, NorthOfCar, Obstacle, OnTriggerEnter, CurbCollision, Detect, Stoplight - Copy Renamed Rotate -> HeadRotate (unused), Follow -> CameraFollow

--- Unity Prototype/Assets/Scripts/CameraFollow.cs
+++ Unity Prototype/Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform focus;
 
--- Unity Prototype/Assets/Scripts/HeadRotate.cs
+++ Unity Prototype/Assets/Scripts/HeadRotate.cs
@@ -10,7 +10,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class HeadRotate : MonoBehaviour
 {
 
     [SerializeField]
