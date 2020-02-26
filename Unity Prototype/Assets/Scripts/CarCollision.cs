6c830727e1f4fb047611a637f1de3c9b866386d2
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-02-26 10:20:41 -0800

    Migrated Collision Audio back to Car Collision
    It seems the merge took my original collision changes out of the script, so I just put them back.
    
    Also, ignore the changes to KeyboardControls. I was messing with it to get the car to drive.

--- Unity Prototype/Assets/Scripts/CarCollision.cs
+++ Unity Prototype/Assets/Scripts/CarCollision.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class CarCollision : MonoBehaviour
 {
     private Rigidbody2D body;
-    private AudioClip bump;
-    public GameObject hitsound;
+    //private AudioClip bump;
+    //public GameObject hitsound;
     private Control1D controlFunctions;
+
+    //collision sounds
+    AudioSource[] characterSounds;
+    AudioSource charOnPed;
+    AudioSource charOnGuard;
+    AudioSource charOnCar;
 
     // Start is called before the first frame update
     void Start()
     {
-        controlFunctions = GetComponent<Control1D>();
-        bump = Resources.Load<AudioClip>("Audio/hit");
+        //controlFunctions = GetComponent<Control1D>();
+        //bump = Resources.Load<AudioClip>("Audio/hit");
+
+        //gets the sounds from whatever gameobject this script is attached to
+        characterSounds = GetComponents<AudioSource>();
+
+        //checks to see if there are at least 3 audio sources
+        if (characterSounds.Length < 3)
+        {
+            Debug.Log("The main character is missing audio sources. Collision SFX may not behave properly");
+        }
+
+        //Remember to ask Sound to start all collision audio with CoX, where X is C, G, or P depending on what is being crashed into
+        charOnPed = ComponentAudioSearch('P');
+        charOnGuard = ComponentAudioSearch('G');
+        charOnCar = ComponentAudioSearch('C');
     }
 
     // Update is called once per frame
@@ -26,9 +46,14 @@ public class CarCollision : MonoBehaviour
     {
         if (col.gameObject.tag == "Car")
         {
-            hitsound.transform.position = transform.position;
-            hitsound.GetComponent<AudioSource>().Play();
+            //hitsound.transform.position = transform.position;
+            //hitsound.GetComponent<AudioSource>().Play();
+            charOnCar.Play();
             CheckErrors.IncrementErrorsAndUpdateDisplay();
+        }
+        if (col.gameObject.tag == "Pedestrian" || col.gameObject.tag == "Stop")
+        {
+            charOnPed.Play();
         }
         if (col.gameObject.tag == "Guardrail")
         {
@@ -41,18 +66,45 @@ public class CarCollision : MonoBehaviour
                 print("else");
                 controlFunctions.blockDirection(-1);
             }
+            charOnGuard.Play();
         }
     }
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "Car")
         {
-            hitsound.transform.position = transform.position;
-            hitsound.GetComponent<AudioSource>().Play();
+            //hitsound.transform.position = transform.position;
+            //hitsound.GetComponent<AudioSource>().Play();
+            charOnCar.Play();
         }
         if (col.gameObject.tag == "Guardrail")
         {
             controlFunctions.blockDirection(0);
+            charOnGuard.Stop();
         }
+    }
+
+    //This method locates audioclips based on the convention 'CoX', where X is the character of the type of thing being collided with
+    AudioSource ComponentAudioSearch(char findme)
+    {
+        AudioSource target = null;
+
+        for (int i = 0; i <= characterSounds.Length - 1; ++i)
+        {
+            //If CoX is followed, X will always be in the 2nd index
+            if (characterSounds[i].clip.name[2] == findme)
+            {
+                target = characterSounds[i];
+                i = characterSounds.Length + 3;
+            }
+        }
+
+        if (target == null)
+        {
+            //Keep in mind that, if this method can't find the clip, the reference is set to null. This will cause problems.
+            Debug.Log("Audio clip for collision type " + findme + " couldn't be found. Collision SFX may not behave properly");
+        }
+
+        return target;
     }
 }
