58edb55109f67fce2e06296d64fe9bf01cfb0932
sean <seanim0920@gmail.com>
2020-03-02 02:37:55 -0800

    Included title screen and instruction music and sfx for entering the game Included intro cutscene to the game Made a copy of the echoey windy noise from the original track but lower pitched (may be easier on the ears) Included wheel controls upon starting the game

---
+++ Unity Prototype/Assets/IntroCutscene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCutscene : MonoBehaviour
+{
+    public AudioSource intro;
+    private PlayerControls controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startLevel());
+        controls = GetComponent<PlayerControls>();
+    }
+
+    IEnumerator startLevel()
+    {
+        intro.Play();
+        yield return new WaitForSeconds(intro.clip.length);
+        controls.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
