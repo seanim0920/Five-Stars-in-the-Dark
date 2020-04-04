3e957515dc918013e2b255dfe93b97f563cd2a50
Brian_Chu <bcchu@ucsc.edu>
2020-04-04 00:10:17 -0700

    ScreenWipe menu/ Lvl Selection

---
+++ Unity Prototype/Assets/Scripts/DisplayTitle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTitle : MonoBehaviour
+{
+
+    public GameObject Instructions;
+	public GameObject LvlSelect;
+	public GameObject Blackout;
+	public bool f = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		f = Masterkey.egg;
+	    var pos = transform.position;
+		
+		if(transform.position.x >= -100) {
+			Instructions.SetActive(false);
+			LvlSelect.SetActive(false);
+			if(f) {
+				Blackout.SetActive(true);
+				LoadScene.Loader("Level 1");
+			}
+		}
+    }
+}
---
+++ Unity Prototype/Assets/Scripts/Masterkey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Masterkey : MonoBehaviour
+{
+	public Button start;
+	public Button level;
+	public Button levelBack;
+	public static bool egg = false;
+	public static bool lvl = false;
+    // Start is called before the first frame update
+     void Start()
+    {
+		Button e = start.GetComponent<Button>();
+		Button l = level.GetComponent<Button>();
+		Button lb = levelBack.GetComponent<Button>();
+		e.onClick.AddListener(TaskStart);
+		l.onClick.AddListener(TaskLvl);
+		lb.onClick.AddListener(TaskLvlReset);
+    }
+	
+	void TaskStart(){
+		egg = true;
+	}
+	
+	void TaskLvl(){
+		lvl = true;
+	}
+	
+	void TaskLvlReset(){
+		lvl = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
