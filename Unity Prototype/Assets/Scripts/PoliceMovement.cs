3ef0640f8b56950fe9f671db12dd5774778230d9
Charles Advincula <cadvincu@ucsc.edu>
2020-04-05 16:38:08 -0700

    Forgot to add in-progress level 2 scene + police movement script

---
+++ Unity Prototype/Assets/Scripts/PoliceMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceMovement : MonoBehaviour
+{
+    public AudioSource siren;
+    private float movementSpeed = 1f;
+    public float neutralSpeed = 1f;
+    private float maxSpeed = 1f;
+    private float acceleration = 0f;
+    // private AudioSource engineSound;
+    // private float eyesight = 3;
+    private Vector3 movementDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementDirection = transform.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        drive();
+    }
+
+    void drive()
+    {
+        transform.position += movementDirection * movementSpeed;
+    }
+}
