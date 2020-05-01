1c36a5b755cb733407bb97e1661c8031cf44860c
Charles Advincula <chadvincula7636428@gmail.com>
2020-05-01 00:30:52 -0700

    Got started on BGM volume control

---
+++ Unity Prototype/Assets/Scripts/Sound/ChangeVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ChangeVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer menuMaster;
+    [SerializeField] private AudioMixer carDialogue;
+
+    public void SetVolume(float newVolume)
+    {
+        menuMaster.SetFloat("Menu BGM", Mathf.Log10(newVolume) * 20);
+        carDialogue.SetFloat("Dialogue Volume", Mathf.Log10(newVolume) * 20);
+    }
+}
