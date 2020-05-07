8c532296c5cafdf36e46219152bc0ad295bdcd69
sean <seanim0920@gmail.com>
2020-05-07 00:05:54 -0700

    LoadScene is updated to remove the error-causing unload line. Menu uses newer version of theme. Something in level 3 changed but idk what. Settings button uses a gear icon on main menu now.

--- Unity Prototype/Assets/Scripts/UI/Scene Managment/LoadScene.cs
+++ Unity Prototype/Assets/Scripts/UI/Scene Managment/LoadScene.cs
@@ -6,11 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public static int prevLevel = 0;
-    //Loads a Unity Scene by exact name. All other scenes will be unloaded
     public static void Loader(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
     }
 }
