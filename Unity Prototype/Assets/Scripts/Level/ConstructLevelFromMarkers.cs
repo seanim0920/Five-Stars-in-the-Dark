d4b1b12b216716d6823d71ce46d74711cf3c2f88
sean <seanim0920@gmail.com>
2020-05-09 04:12:28 -0700

    Constructlevel checks if gamepad is connected before checking if "buttonsouth" is pressed when setting skipSection to true subtitleText.cs checks if the current subtitle in constructLevel is not empty

--- Unity Prototype/Assets/Scripts/Level/ConstructLevelFromMarkers.cs
+++ Unity Prototype/Assets/Scripts/Level/ConstructLevelFromMarkers.cs
@@ -577,7 +577,7 @@ public class ConstructLevelFromMarkers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("s") || Gamepad.current.buttonSouth.isPressed)
+        if (Input.GetKeyDown("s") || (Gamepad.current != null && Gamepad.current.buttonSouth.isPressed))
         {
             skipSection = true;
         }
