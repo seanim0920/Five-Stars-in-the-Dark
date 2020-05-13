8b0dd01a34bf8aa588fea097dc024ad880706c10
sean <seanim0920@gmail.com>
2020-05-12 20:25:27 -0700

    Updated end screen music

--- Five Stars in the Dark/Assets/Scripts/UI/Scene Managment/EndScene.cs
+++ Five Stars in the Dark/Assets/Scripts/UI/Scene Managment/EndScene.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class EndScene : MonoBehaviour
 {
-    public AudioSource bass;
-    public AudioSource drums;
     public GameObject fade;
 	public GameObject panel;
     public DisplayScore scoreScript;
     // Start is called before the first frame update
     void Start()
     {
-        bass.time = 16;
-        drums.time = 16;
-        bass.Play();
-        drums.Play();
         fade.SetActive(true);
         // panel.SetActive(false);
         // scoreScript.enabled = true;
