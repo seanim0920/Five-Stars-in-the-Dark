226dcf4117bb4602178bf2ab9fffc3b388ff85df
sean <seanim0920@gmail.com>
2020-05-18 02:42:28 -0700

    Stoplight repeatedly loops, rather than shutting off after 1 round of cars now.

--- Five Stars in the Dark/Assets/Scripts/Level/Obstacles/Stoplight.cs
+++ Five Stars in the Dark/Assets/Scripts/Level/Obstacles/Stoplight.cs
@@ -19,21 +19,25 @@ public class Stoplight : MonoBehaviour
         transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
 
         transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
-        for (int loop = 0; loop < loopAmount; loop++)
+        while (true)
         {
-            for (int i = 0; i < pattern.Length; i++)
+            for (int loop = 0; loop < loopAmount; loop++)
             {
-                if (pattern[i] == 'w')
+                for (int i = 0; i < pattern.Length; i++)
                 {
-                    GameObject npc = Instantiate(Resources.Load<GameObject>(objectDirectory + "StoplightCar"), transform);
-                    npc.GetComponent<Rigidbody2D>().isKinematic = true;
-                    npc.GetComponent<NPCMovement>().neutralSpeed = 2;
-                    npc.transform.localPosition = transform.GetChild(0).localPosition;
-                    npc.transform.Rotate(0, 0, -90);
-                    Destroy(npc, 8);
+                    if (pattern[i] == 'w')
+                    {
+                        GameObject npc = Instantiate(Resources.Load<GameObject>(objectDirectory + "StoplightCar"), transform);
+                        npc.GetComponent<Rigidbody2D>().isKinematic = true;
+                        npc.GetComponent<NPCMovement>().neutralSpeed = 2;
+                        npc.transform.localPosition = transform.GetChild(0).localPosition;
+                        npc.transform.Rotate(0, 0, -90);
+                        Destroy(npc, 8);
+                    }
+                    yield return new WaitForSeconds(1f);
                 }
-                yield return new WaitForSeconds(1f);
             }
+            yield return new WaitForSeconds(8f);
         }
         tag = "Go";
         transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
