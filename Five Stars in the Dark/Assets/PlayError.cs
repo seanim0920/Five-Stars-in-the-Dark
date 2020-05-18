965869a3ed6a236a11368c25a0fb6d3b39df811f
sean <seanim0920@gmail.com>
2020-05-18 03:32:38 -0700

    Removed unneeded extra parameter for playerror

--- Five Stars in the Dark/Assets/PlayError.cs
+++ Five Stars in the Dark/Assets/PlayError.cs
@@ -17,7 +17,7 @@ public class PlayError : MonoBehaviour
         
     }
 
-    public static IEnumerator PauseDialogue(AudioClip passengerHurt, float x)
+    public static IEnumerator PauseDialogue(AudioClip passengerHurt)
     {
         Debug.Log("Pausing Dialogue");
         AudioSource dialogue = ConstructLevelFromMarkers.levelDialogue;
