18c626a5961356ab2ab6d13375ca526e87dbce23
Charles Advincula <chadvincula7636428@gmail.com>
2020-05-24 17:20:43 -0700

    Added Tableaux mockups for strafing and guardrail collisions

---
+++ Five Stars in the Dark/Assets/Scripts/Level/StartTutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class StartTutorial : MonoBehaviour
+{
+    private AudioSource levelAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelAudio = gameobject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelAudio.time >= levelAudio.clip.length)
+        {
+            Instantiate()
+        }
+    }
+}
